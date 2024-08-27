1800df9b9817bd97d189444d5979c485921abcf9
eltonanit <elton.ani@gmail.com>
2024-08-27 16:48:56 +0200

    [UPD] Added Movieslist

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
- 
 
 <script>
-import AppHeader from './components/AppHeader.vue';
- 
-
+import { store } from './store.js'
+import SearchBar from './components/SearchBar.vue'
+import MovieList from './components/MovieList.vue'
+import axios from 'axios'
 
 export default {
-  components:{
-    AppHeader
+  name: 'App',
+  components: {
+    SearchBar,
+    MovieList
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    searchMovies(query) {
+      axios.get(`${store.apiBaseUrl}/search/movie`, {
+        params: {
+          api_key: store.apiKey,
+          query: query
+        }
+      })
+        .then(response => {
+          store.movies = response.data.results
+        })
+        .catch(error => {
+          console.error('Error fetching movies:', error)
+        })
+    }
   }
-  
 }
 </script>
 
-<template lang="">
-  <AppHeader />
- 
-
+<template>
+  <div id="app">
+    <h1>Movie Search</h1>
+    <SearchBar @search="searchMovies" />
+    <MovieList :movies="store.movies" />
+  </div>
 </template>
-
-<style lang="scss">
-
-@import './style/general.scss'
-  
-</style>
- 
---
+++ src/components/MovieList.vue
@@ -0,0 +1,21 @@
+
+
+<script>
+export default {
+    name: 'MovieList',
+    props: {
+        movies: Array
+    }
+}
+</script>
+
+<template>
+    <div class="movie-list">
+        <div v-for="movie in movies" :key="movie.id" class="movie-item">
+            <h2>{{ movie.title }}</h2>
+            <p>Titolo Originale: {{ movie.original_title }}</p>
+            <p>Lingua: {{ movie.original_language }}</p>
+            <p>Voto: {{ movie.vote_average }}</p>
+        </div>
+    </div>
+</template>
