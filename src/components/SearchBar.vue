8ab54d80c42530b12bf78cff3207caee9b8e3952
eltonanit <elton.ani@gmail.com>
2024-08-27 16:45:26 +0200

    [UPD] Added Searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'SearchBar',
+    data() {
+        return {
+            searchQuery: ''
+        }
+    },
+    methods: {
+        search() {
+            this.$emit('search', this.searchQuery)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="search-bar">
+        <input v-model="searchQuery" type="text" placeholder="Cerca un film...">
+        <button @click="search">Cerca</button>
+    </div>
+</template>
